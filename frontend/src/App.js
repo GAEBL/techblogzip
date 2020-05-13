9826917c51a1ee6469d83b78456cb12056b5f0da
mandaringit <mandarin.dev@kakao.com>
2020-05-13 17:21:25 +0900

    feat: 기본 레이아웃 구현 및 스타일 (S02P31C103-42)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,39 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import TrendPage from './pages/TrendPage';
 import NotFoundPage from './pages/NotFoundPage';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const AppGridWrapper = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  /* 헤더높이, 컨텐츠 전체, 푸터 높이 */
+  grid-template-rows: 3.5rem auto 3rem;
+`;
+
+const Contents = styled.section`
+  padding: 1rem;
+`;
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={MainPage} exact />
-      <Route path="/login" component={LoginPage} exact />
-      <Route path="/register" component={RegisterPage} exact />
-      <Route path="/posts" component={RecentPostsPage} exact />
-      <Route path="/search/:query" component={SearchResultPage} exact />
-      <Route path="/trend" component={TrendPage} exact />
-      <Route path="*" component={NotFoundPage} />
-    </Switch>
+    <AppGridWrapper>
+      <Header />
+      <Contents>
+        <Switch>
+          <Route path="/" component={MainPage} exact />
+          <Route path="/login" component={LoginPage} exact />
+          <Route path="/register" component={RegisterPage} exact />
+          <Route path="/posts" component={RecentPostsPage} exact />
+          <Route path="/search/:query" component={SearchResultPage} exact />
+          <Route path="/trend" component={TrendPage} exact />
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
+      </Contents>
+      <Footer />
+    </AppGridWrapper>
   );
 }
 
