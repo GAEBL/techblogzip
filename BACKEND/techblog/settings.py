008912a178409d5510bc61608abec029f74a40a2
coffeetrip <yye7926@naver.com>
2020-05-13 14:21:42 +0900

    feat:django secret key hide (S02P31C103-18)

--- BACKEND/techblog/settings.py
+++ BACKEND/techblog/settings.py
@@ -12,16 +12,24 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import datetime 
+import sys
+import json
+from django.core.exceptions import ImproperlyConfigured
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+with open("secrets.json") as f:
+    secrets = json.loads(f.read())
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i)^^nthnwnslx)b-v%w)no%aj=rv^--lg(d%i7bia#_lvse@+$'
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+ 
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
