7c79760e044dbc2de24d4751eb644c1fc1caf63d
coffeetrip <yye7926@naver.com>
2020-05-14 09:59:47 +0900

    feat: backend login (S02P31C103-22)

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404
+from django.contrib import auth
+from django.contrib.auth.hashers import check_password
 
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_jwt.settings import api_settings
 
 from .serializers import UserSerializer
 from .models import User
@@ -13,7 +16,6 @@ from .models import User
 @permission_classes([AllowAny,])
 def signup(request):
     serializer = UserSerializer(data=request.POST)  
-    
     # 1.입력받은 password를 암호화하기 위해 
     if serializer.is_valid(raise_exception=True):
         password = serializer.validated_data.get('password')
@@ -30,5 +32,16 @@ def signup(request):
 @api_view(['POST'])
 @permission_classes([AllowAny, ])
 def login(request):
-    return
+    username = request.POST['username']
+    password = request.POST['password']
+    user = get_object_or_404(User, username=username)
+    if check_password(password, user.password) == True:
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER   
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        return JsonResponse({'result': 'ture', 'token': token})
+    else:
+        return HttpResponse(status=400)
+    
 
