178cfa729bb125dd7585bbd975b66c12d0ab5112
mandaringit <mandarin.dev@kakao.com>
2020-05-18 15:54:49 +0900

    feat: theme 추가 및 연결

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './style/theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
