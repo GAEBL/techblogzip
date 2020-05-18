59dccbc9ce518e715da32945aa8ee699709c6573
coffeetrip <yye7926@naver.com>
2020-05-18 14:58:33 +0900

    refactor: user email model add

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -15,6 +15,7 @@ from .models import User
 @api_view(['POST'])
 @permission_classes([AllowAny,])
 def signup(request):
+    print(request.POST)
     serializer = UserSerializer(data=request.POST)  
     # 1.입력받은 password를 암호화하기 위해 
     if serializer.is_valid(raise_exception=True):
