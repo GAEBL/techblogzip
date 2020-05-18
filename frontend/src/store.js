ca0480528f4f5032ce83cfbdbe2dbdd526e5b4dc
mandaringit <mandarin.dev@kakao.com>
2020-05-18 10:58:51 +0900

    feat: redux-thunk 추가 및 적용

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer';
+import ReduxThunk from 'redux-thunk';
 
 // 리덕스 스토어를 생성합니다. 스토어는 단 하나입니다!
 export const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware()),
+  composeWithDevTools(applyMiddleware(ReduxThunk)),
 );
