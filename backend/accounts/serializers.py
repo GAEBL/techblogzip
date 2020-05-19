5cbb1603a23f9a7d1680f1b6f3f3f3f830d390ed
coffeetrip <yye7926@naver.com>
2020-05-19 13:45:52 +0900

    refactor: accouts > login

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,8 +5,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'password', 'is_subscribed']
-
-class SignupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ['id', 'username',]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework_jwt.settings import api_settings
 
-from .serializers import UserSerializer, SignupSerializer
+from .serializers import UserSerializer
 from .models import User
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -31,8 +31,7 @@ def signup(request):
         payload = jwt_payload_handler(user)
         token = jwt_encode_handler(payload)
 
-        serializer = SignupSerializer(user)
-        return JsonResponse({'token':token, 'user':serializer.data,})
+        return JsonResponse({'token':token, 'user':{'id':user.id,'username':user.username}})
     return HttpResponse(status=400)
 
 
@@ -46,7 +45,7 @@ def login(request):
     if check_password(password, user.password) == True: 
         payload = jwt_payload_handler(user)
         token = jwt_encode_handler(payload)
-        return JsonResponse({'result': 'true', 'token': token})
+        return JsonResponse({'token': token, 'user':{'id':user.id,'username':user.username}})
     else:
         return HttpResponse(status=400)
     
