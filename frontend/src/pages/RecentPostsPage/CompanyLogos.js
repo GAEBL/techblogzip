9aefa1024daa293e5aec95e6a9e970485106970a
vxda7 <vxda77@gmail.com>
2020-05-19 09:52:43 +0900

    refactor: recentpost UI refactoring

---
+++ frontend/src/pages/RecentPostsPage/CompanyLogos.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LogoImg = styled.img`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
+
+const LogoButton = styled.button`
+  background-color: ${(props) => props.color};
+  border: solid black 1px;
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const CompanyLogos = () => {
+  const companys = [
+    ['brother_logo.png', 'white'],
+    ['coupang_logo.png', 'black'],
+    ['d2_logo.gif', 'white'],
+    ['kakao_logo.jpg', 'black'],
+    ['line_logo.png', 'black'],
+    ['sds_logo.png'],
+    ['spoqa_logo.png'],
+    ['tmon_logo.png'],
+    ['toast_logo.png'],
+    ['yanolja_logo.png', 'black'],
+  ];
+  return (
+    <div>
+      {companys.map((company, index) => (
+        <LogoButton key={index} color={company[1]}>
+          <LogoImg
+            width={'auto'}
+            height={'20px'}
+            src={process.env.PUBLIC_URL + '/images/' + company[0]}
+            alt=""
+          />
+        </LogoButton>
+      ))}
+    </div>
+  );
+};
+
+export default CompanyLogos;
