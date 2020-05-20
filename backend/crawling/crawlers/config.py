b5e211b51f3dc51a10809127f34ea5f0b14ba279
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-05-20 14:49:36 +0900

    refactor: crawler config 설정 변경

--- backend/crawling/crawlers/config.py
+++ backend/crawling/crawlers/config.py
@@ -6,10 +6,14 @@ options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument('lang=ko-KR')
 options.add_argument(
-    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-)  # 주의: 리눅스에서 변경할 필요
+    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36')
 
 PATH = os.getcwd().replace('\\', '/')
 driver = webdriver.Chrome(
-    f'{PATH}/crawling/crawlers/chromedriver.exe', options=options
-)
+    f'{PATH}/crawling/crawlers/chromedriver.exe', options=options)
+
+CSS_SELECTOR = driver.find_elements_by_css_selector
+
+COMPANY = 'matcher'
+ERROR_MESSAGE = {'status': 400, 'message': 'URL을 불러올 수 없습니다. 나중에 다시 시도해주세요.'}
+SUCESS_MESSAGE = {'status': 200, 'message': f'정상적으로 게시물을 수집했습니다.'}
