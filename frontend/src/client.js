096a274d66b7381e0439189df1dcab0187e55b33
mandaringit <mandarin.dev@kakao.com>
2020-05-20 11:19:04 +0900

    feat: API에 토큰 추가, 인증시 각종 로직 미들웨어 추가(S02P31C103-75)

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,14 +2,18 @@ import axios from 'axios';
 
 const client = axios.create();
 
-client.defaults.baseURL = 'http://localhost:8000';
+// LOCAL
+client.defaults.baseURL = 'http://192.168.0.15:8080';
 
+// PRODUCT
 // 기본적으로 API를 요청할때 , 토큰 존재시 헤더에 붙여주는 로직
-/**
-  let token = localStorage.getItem('token') || null;
+export function setAuthorization() {
+  let token = sessionStorage.getItem('jwt') || null;
   if (token) {
-    client.defaults.headers.common['Authorization'] = token;
-  }  
- */
+    client.defaults.headers.common['Authorization'] = `JWT ${token}`;
+  } else {
+    delete client.defaults.headers.common['Authorization'];
+  }
+}
 
 export default client;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer';
 import ReduxThunk from 'redux-thunk';
+import { AddAuthorizationMiddleware, setAuthInfo } from './middleware';
 
 // 리덕스 스토어를 생성합니다. 스토어는 단 하나입니다!
 export const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(ReduxThunk)),
+  composeWithDevTools(
+    applyMiddleware(AddAuthorizationMiddleware, setAuthInfo, ReduxThunk),
+  ),
 );
