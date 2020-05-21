d521ba9aab63f0e068f8aa8ef70c6c47bbfe9996
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-05-21 13:24:18 +0900

    refactor: KAKAO TECH 기능 최적화 (S02P31C103-XX)

--- backend/crawling/crawlers/kakao_crawler.py
+++ backend/crawling/crawlers/kakao_crawler.py
@@ -1,17 +1,20 @@
-from .config import driver
+from .config import driver, ERROR_MESSAGE, SUCESS_MESSAGE, CSS_SELECTOR
 from mainapp.models import Company, Post
 from tqdm import tqdm
-import time
 
 
-def get_contents(driver_selector, kakao):
-    posts = Post.objects.filter(company=kakao, contents='')
+def get_contents(company):
+    posts = Post.objects.filter(company=company, contents='')
     for post in tqdm(posts):
-        driver.get(post.url)
-        time.sleep(10)
+        try:
+            driver.get(post.url)
+            driver.implicitly_wait(30)
+        except:
+            ERROR_MESSAGE['company'] = company.name
+            return ERROR_MESSAGE
 
-        contents = driver_selector('div.post-content')[0]
-        articles = contents.find_elements_by_css_selector('p')
+        div = CSS_SELECTOR('div.post-content')[0]
+        articles = div.find_elements_by_css_selector('p')
 
         contents = ''
         for article in articles:
@@ -20,45 +23,44 @@ def get_contents(driver_selector, kakao):
         post.contents = contents
         post.save()
 
+    SUCESS_MESSAGE['company'] = company.name
+    return SUCESS_MESSAGE
+
 
 def get_posts(url):
-    driver_selector = driver.find_elements_by_css_selector
     kakao = Company.objects.get(name='KAKAO TECH')
 
     cnt = 1
     while True:
         try:
             driver.get(url)
-            time.sleep(10)
+            driver.implicitly_wait(30)
         except:
-            return {'status': 500, 'message': 'Crawling을 할 수 없습니다. 해당 페이지의 주소와 서버 상태를 확인하세요.'}
+            ERROR_MESSAGE['company'] = kakao.name
+            return ERROR_MESSAGE
+
+        posts = CSS_SELECTOR('ul.list_post')
+        posts = posts[0].find_elements_by_css_selector('li')
+        for post in posts:
+            element_selector = post.find_element_by_css_selector
+
+            title = element_selector('strong.tit_post').text
+            if len(Post.objects.filter(company=kakao, title=title)) > 0:
+                return get_contents(kakao)
+
+            date = element_selector('span.txt_date').text
+            url = element_selector('a.link_post').get_attribute('href')
+            try:
+                image = element_selector('img').get_attribute('src')
+            except:
+                image = ''
+
+            Post.objects.get_or_create(
+                company=kakao, title=title, contents='', date=date, image=image, url=url)
+
+        get_next = CSS_SELECTOR('a.next.page-numbers')
+        if get_next:
+            cnt += 1
+            url = f'https://tech.kakao.com/blog/page/{cnt}/'
         else:
-            posts = driver_selector('ul.list_post')
-            posts = posts[0].find_elements_by_css_selector('li')
-            for post in posts:
-                element_selector = post.find_element_by_css_selector
-
-                title = element_selector('strong.tit_post').text
-                if len(Post.objects.filter(company=kakao, title=title)) > 0:
-                    get_contents(driver_selector, kakao)
-                    return {'status': 200, 'message': 'KAKAO TECH에 대한 Crawling을 완료했습니다.'}
-
-                date = element_selector('span.txt_date').text
-                url = element_selector('a.link_post').get_attribute('href')
-                try:
-                    image = element_selector('img').get_attribute('src')
-                except:
-                    image = ''
-
-                _, is_created = Post.objects.get_or_create(
-                    company=kakao, title=title, contents='', date=date,
-                    image=image, url=url
-                )
-
-            is_ended = len(driver_selector('a.next.page-numbers')) == 0
-            if is_ended == False:
-                cnt += 1
-                url = f'https://tech.kakao.com/blog/page/{cnt}/'
-            else:
-                get_contents(driver_selector, kakao)
-                return {'status': 200, 'message': 'KAKAO TECH에 대한 Crawling을 완료했습니다.'}
+            return get_contents(kakao)
