4606ba7127bc433215b44c403dd5e641807e2298
vxda7 <vxda77@gmail.com>
2020-05-27 10:26:36 +0900

    refactor: 리듀서에서 search제거

--- frontend/src/reducer.js
+++ frontend/src/reducer.js
@@ -3,12 +3,10 @@ import loading from './reducers/loading';
 import auth from './reducers/auth';
 import user from './reducers/user';
 import post from './reducers/post';
-import search from './reducers/search';
 
 export default combineReducers({
   loading,
   auth,
   user,
   post,
-  search,
 });
