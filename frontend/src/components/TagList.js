dd5cd820226480cc11fcf9fcbdd4b9777d4dc446
mandaringit <mandarin.dev@kakao.com>
2020-05-30 18:00:00 +0900

    refactor: 태그 스타일 수정

--- frontend/src/components/TagList.js
+++ frontend/src/components/TagList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { colors } from '@material-ui/core';
 
 const TagWrapper = styled.div`
   display: flex;
@@ -7,21 +8,18 @@ const TagWrapper = styled.div`
 `;
 
 const Tag = styled.span`
-  background-color: ${({ theme }) => theme.mainColor};
+  background-color: ${colors.deepOrange[100]};
+  font-size: 0.8rem;
   padding: 0.1rem 0.3rem;
-  color: white;
-  border-radius: 5px;
-  margin-bottom: 0.2rem;
-  & + & {
-    margin-left: 0.3rem;
-  }
+  color: ${colors.deepOrange[900]};
+  margin: 0.2rem 0.3rem 0 0;
 `;
 
 function TagList({ tags }) {
   return (
     <TagWrapper>
       {tags.map((tag, i) => (
-        <Tag key={i}>{tag.name}</Tag>
+        <Tag key={i}>#{tag.name}</Tag>
       ))}
     </TagWrapper>
   );
