c5f596cbbd4ba23fa2313dd22345da6d05f2c537
mandaringit <mandarin.dev@kakao.com>
2020-05-30 18:16:23 +0900

    refactor: 최신글 페이지 디자인 및 로직 수정

--- frontend/src/pages/RecentPostsPage/index.js
+++ frontend/src/pages/RecentPostsPage/index.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import Companylogo from './CompanyLogo';
+import LogoButton from './LogoButton';
 import { NativeSelect, FormControl } from '@material-ui/core';
-import Pagination from '@material-ui/lab/Pagination';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getAllPosts, clearPosts } from '../../reducers/post';
 import PostList from '../../components/PostList';
 import SimplePagination from '../../components/Material/SimplePagination';
-
-const CompanySelector = styled.div`
-  h1 {
-    margin-bottom: 1rem;
-  }
-`;
+import companyLogoData from '../../lib/companyLogoData';
 
 const RecentPostsPageWrapper = styled.div`
   max-width: ${({ theme }) => theme.maxPageWidth};
@@ -20,7 +14,15 @@ const RecentPostsPageWrapper = styled.div`
   padding: 1rem;
 `;
 
-const PostsDropdown = styled.div`
+const Title = styled.h1`
+  margin-bottom: 1rem;
+`;
+
+const CompanySelector = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const DropDownBar = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 0.5rem;
@@ -53,7 +55,7 @@ function RecentPostsPage() {
   const handleClick = (company) => {
     setQueryParams({
       ...queryParams,
-      company: companyhash[company],
+      company,
       page: 1,
     });
   };
@@ -67,30 +69,30 @@ function RecentPostsPage() {
 
   return (
     <RecentPostsPageWrapper>
+      <Title>기업의 기술블로그에서 원하는 주제를 찾아보세요.</Title>
       <CompanySelector>
-        <h1>기업의 기술블로그에서 원하는 주제를 찾아보세요.</h1>
-        {companys.map((company, index) => (
-          <Companylogo
-            key={index}
-            fileName={company[0]}
-            color={company[1]}
-            handleClick={() => handleClick(company[0])}
+        {companyLogoData.map((company) => (
+          <LogoButton
+            key={company.name}
+            company={company}
+            selected={queryParams.company}
+            handleClick={() => handleClick(company.name)}
           />
         ))}
       </CompanySelector>
 
-      <PostsDropdown>
-        <h1>포스트</h1>
-        <FormControl>
-          <NativeSelect value={queryParams.sort} onChange={handleChange}>
-            <option value="">최신순</option>
-            <option value="likes">좋아요순</option>
-            <option value="user_recommendation">매칭순</option>
-          </NativeSelect>
-        </FormControl>
-      </PostsDropdown>
-
-      <PostList actionType="post/GET_ALL_POSTS" />
+      <PostList actionType="post/GET_ALL_POSTS">
+        <DropDownBar>
+          <Title>포스트</Title>
+          <FormControl>
+            <NativeSelect value={queryParams.sort} onChange={handleChange}>
+              <option value="">최신순</option>
+              <option value="likes">좋아요순</option>
+              <option value="user_recommendation">매칭순</option>
+            </NativeSelect>
+          </FormControl>
+        </DropDownBar>
+      </PostList>
 
       <SimplePagination
         currentPage={queryParams.page}
@@ -101,27 +103,3 @@ function RecentPostsPage() {
 }
 
 export default RecentPostsPage;
-
-const companys = [
-  ['brother_logo.png', 'white'],
-  ['coupang_logo.png', 'black'],
-  ['d2_logo.gif', 'white'],
-  ['kakao_logo_black.png', 'black'],
-  ['line_logo.png', 'black'],
-  ['sds_logo.png', 'white'],
-  ['spoqa_logo.png', 'white'],
-  ['toast_logo.png', 'white'],
-  ['yanolja_logo.png', 'black'],
-];
-
-const companyhash = {
-  'brother_logo.png': 'WOOWABROS',
-  'coupang_logo.png': 'COUPANG TECH',
-  'd2_logo.gif': 'NAVER D2',
-  'kakao_logo_black.png': 'KAKAO TECH',
-  'line_logo.png': 'LINE ENGINEERING',
-  'sds_logo.png': 'SAMSUNG SDS',
-  'spoqa_logo.png': 'SPOQA',
-  'toast_logo.png': 'TOAST',
-  'yanolja_logo.png': 'YANOLJA',
-};
