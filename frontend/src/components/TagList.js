6b29167426384bf2e4f872bb8926cf66e00d6e16
mandaringit <mandarin.dev@kakao.com>
2020-05-31 10:40:56 +0900

    feat: 태그 리스트 링크 추가 (S02P31C103-150)

--- frontend/src/components/TagList.js
+++ frontend/src/components/TagList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { colors } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const TagWrapper = styled.div`
   display: flex;
@@ -8,18 +8,24 @@ const TagWrapper = styled.div`
 `;
 
 const Tag = styled.span`
-  background-color: ${colors.deepOrange[100]};
   font-size: 0.8rem;
   padding: 0.1rem 0.3rem;
-  color: ${colors.deepOrange[900]};
   margin: 0.2rem 0.3rem 0 0;
+  background-color: ${({ theme }) => theme.nonselectedMainColor[100]};
+  color: ${({ theme }) => theme.nonselectedMainColor[900]};
+`;
+
+const TagLink = styled(Link)`
+  text-decoration: none;
 `;
 
 function TagList({ tags }) {
   return (
     <TagWrapper>
       {tags.map((tag, i) => (
-        <Tag key={i}>#{tag.name}</Tag>
+        <TagLink to={`/tag/${tag.name}`}>
+          <Tag key={i}>#{tag.name}</Tag>
+        </TagLink>
       ))}
     </TagWrapper>
   );
