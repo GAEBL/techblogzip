1130761930049cd016c3d6546a65a4011e2ac7de
mandaringit <mandarin.dev@kakao.com>
2020-06-02 17:06:09 +0900

    refactor: 백엔드 루트 API 수정

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const client = axios.create();
 
 // LOCAL
-client.defaults.baseURL = 'http://192.168.0.15:8080';
+client.defaults.baseURL = 'http://k02c1031.p.ssafy.io:8000';
 
 // PRODUCT
 // 기본적으로 API를 요청할때 , 토큰 존재시 헤더에 붙여주는 로직
