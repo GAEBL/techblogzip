6ce7a54817843a1192589db47035437a9923bf0d
coffeetrip <yye7926@naver.com>
2020-06-02 09:28:36 +0900

    feat: /trend/ targetData에 해당하는 json결과 보내기 (S02P31C103-171)

--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import operator
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.db.models import Q, Count
@@ -148,22 +150,54 @@ def trend(request):
     company = request.query_params.get('company')
     start_date = request.query_params.get('startdate')
     end_date = request.query_params.get('enddate')
-    target_data = request.query_params.get('target_data')
+    target_data = request.query_params.get('targetdata')  # backend
+
+    language_tag = [] 
+    lib_tag = []
+    front_tag = ['사회', '과학']
+    backend_tag = ['국어', '수학']
+
+    if target_data == 'language':
+        target_tag = language_tag
+    elif target_data == 'lib':
+        target_tag = lib_tag
+    elif target_data == 'frontend':
+        target_tag = frontend_tag
+    elif target_data == 'backend':
+        target_tag = backend_tag
+
+    query = reduce(operator.or_, (Q(tags__name__icontains=target)
+                                  for target in target_tag))
 
     try:
         company_id = get_object_or_404(Company, name=company).id
-        posts = Post.objects.filter(company=company_id, date__range=[
+        posts = Post.objects.filter(query, company=company_id, date__range=[
                                     start_date, end_date]).order_by('-date')
     except:
         company_id = 0
         posts = Post.objects.filter(
             date__range=[start_date, end_date]).order_by('-date')
 
-    all_query_count, last_page, results = pagination(posts, request)
-    serializer = PostSerializer(
-        results, context={'request': request}, many=True)
+    tag_count = {}
+    if posts.exists():
+        for post in posts.iterator():
+            tags = post.tags
+            if tags.exists():
+                for tag in tags.values():
+                    tag_id = tag['name']  # id, name
+                    if tag_id in tag_count:
+                        tag_count[tag_id] += 1
+                    else:
+                        tag_count[tag_id] = 1
+    else:
+        return JsonResponse({'result': 'noData'})
+    
+    trend_dict = {}
+    for key, val in tag_count.items():
+        trend_dict['name'] = key
+        trend_dict['value'] = val
 
-    return JsonResponse({'lastPage': last_page, 'company': company, 'data': serializer.data})
+    return JsonResponse({'data': tag_count})
 
 
 @api_view(['GET'])
