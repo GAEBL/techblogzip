26afca223243f82b7cd06f583e4742a6fcf984c0
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-06-05 10:15:52 +0900

    refactor: crawler 데이터 추가

--- backend/crawling/crawlers/spoqa_crawler.py
+++ backend/crawling/crawlers/spoqa_crawler.py
@@ -16,7 +16,10 @@ def get_contents(company):
             return ERROR_MESSAGE
 
         div = CSS_SELECTOR('div.post')[0]
-        articles = div.find_elements_by_css_selector('p')
+        articles = div.find_elements_by_css_selector('h1')
+        articles += div.find_elements_by_css_selector('h2')
+        articles += div.find_elements_by_css_selector('h3')
+        articles += div.find_elements_by_css_selector('p')
 
         try:
             image = div.find_element_by_css_selector(
@@ -26,7 +29,8 @@ def get_contents(company):
 
         contents = ''
         for article in articles:
-            contents += article.text + '\n'
+            if not article.find_elements_by_css_selector('code'):
+                contents += article.text + ' '
 
         post.contents = contents
         post.image = image
--- backend/crawling/crawlers/yanolja_crawler.py
+++ backend/crawling/crawlers/yanolja_crawler.py
@@ -1,4 +1,5 @@
 from .config import driver, ERROR_MESSAGE, SUCESS_MESSAGE, CSS_SELECTOR
+from selenium.webdriver.common.by import By
 from mainapp.models import Company, Post
 from tqdm import tqdm
 from time import sleep
@@ -15,7 +16,11 @@ def get_contents(company):
             return ERROR_MESSAGE
 
         div = CSS_SELECTOR('section.post-content')[0]
-        articles = div.find_elements_by_css_selector('p')
+        articles = div.find_elements_by_css_selector('h1')
+        articles += div.find_elements_by_css_selector('h2')
+        articles += div.find_elements_by_css_selector('h3')
+        articles += div.find_elements_by_css_selector('p')
+
         try:
             image = CSS_SELECTOR(
                 'header.post-head')[0].get_attribute('style').split('url("')[1][:-3]
@@ -25,7 +30,8 @@ def get_contents(company):
 
         contents = ''
         for article in articles:
-            contents += article.text + '\n'
+            if not article.find_elements_by_css_selector('code'):
+                contents += article.text + ' '
 
         post.contents = contents
         post.image = image
