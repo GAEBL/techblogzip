46885c249b07c8c2e0e9e711563b82618fd32b73
mandaringit <mandarin.dev@kakao.com>
2020-06-05 11:30:54 +0900

    refactor: 마이페이지 디자인 수정

--- frontend/src/pages/MyPage/index.js
+++ frontend/src/pages/MyPage/index.js
@@ -4,7 +4,27 @@ import styled from 'styled-components';
 import PostList from '../../components/PostList';
 import { getPostsByLiked } from '../../reducers/post';
 import SimplePagination from '../../components/Material/SimplePagination';
-import { Fade } from '@material-ui/core';
+import { Fade, makeStyles } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+const useStyle = makeStyles(() => ({
+  root: {
+    fontSize: '1.5rem',
+    color: '#f44336',
+    scale: '1.3',
+  },
+}));
+
+const HeartIcon = styled(FavoriteIcon)`
+  animation: beat 0.3s infinite alternate;
+  transform-origin: center;
+  /* Heart beat animation */
+  @keyframes beat {
+    to {
+      transform: scale(1.4);
+    }
+  }
+`;
 
 const MyPageWrapper = styled.div`
   max-width: ${({ theme }) => theme.maxPageWidth};
@@ -12,11 +32,17 @@ const MyPageWrapper = styled.div`
   padding: 1rem;
 
   .page__title {
+    font-size: 2rem;
     margin-bottom: 1rem;
+  }
+
+  .page__username {
+    color: ${({ theme }) => theme.mainColor};
   }
 `;
 
 function MyPage({ history }) {
+  const classes = useStyle();
   const { isLoggedIn, currentUser } = useSelector(({ user, post }) => ({
     isLoggedIn: user.isLoggedIn,
     currentUser: user.currentUser,
@@ -39,7 +65,8 @@ function MyPage({ history }) {
           <>
             <PostList actionType="post/GET_POST_BY_LIKED">
               <h1 className="page__title">
-                {currentUser.username}님이 스크랩한 기술 블로그
+                <span className="page__username">{currentUser.username}</span>
+                님이 찜 <HeartIcon classes={classes} /> 한 블로그 포스트
               </h1>
             </PostList>
 
