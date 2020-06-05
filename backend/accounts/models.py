cdc9ae0d3470c66ca38436a292bce188b38f11c5
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-06-05 11:04:18 +0900

    refactor: modify accounts application

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,7 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # email = models.EmailField('email', unique=True)
     email = models.EmailField('email')
     is_subscribed = models.BooleanField(default=False)
-
