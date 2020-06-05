52f711a57322b6f4a9f9303d7c1d91ef99c7f7c0
mandaringit <mandarin.dev@kakao.com>
2020-06-05 11:56:49 +0900

    refactor: 푸터 디자인 수정

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,10 +10,65 @@ const FooterWrapper = styled.footer`
   color: white;
 `;
 
+const InnerBox = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  font-family: 'VT323', monospace;
+  .team__slogan {
+    font-size: 5rem;
+    .accent {
+      color: ${({ theme }) => theme.mainColor};
+    }
+  }
+
+  .maker {
+    font-size: 1rem;
+    .maker__name {
+      color: ${({ theme }) => theme.mainColor};
+    }
+    .maker__role {
+    }
+  }
+
+  .team__name {
+    font-size: 3rem;
+  }
+`;
+
 function Footer(props) {
   return (
     <FooterWrapper>
-      <h3>GAEBL</h3>
+      <InnerBox>
+        <h2 className="team__slogan">
+          <span role="img" aria-label="img">
+            💻 WE
+          </span>{' '}
+          <span className="accent">GAE</span>THERING DEV T
+          <span className="accent">E</span>CH <span className="accent">BL</span>
+          OG{' '}
+        </h2>
+        <div className="maker">
+          <span className="maker__name">KIM SEUNG YEON</span>{' '}
+          <span className="maker__role">[FRONTEND / DEPLOY]</span>
+        </div>
+        <div className="maker">
+          <span className="maker__name">KIM HYEOK JUN</span>{' '}
+          <span className="maker__role">[FRONTEND & REVIEWER]</span>
+        </div>
+        <div className="maker">
+          <span className="maker__name">YANG YE EUN</span>{' '}
+          <span className="maker__role">[BACKEND / UCC DIRECTOR]</span>
+        </div>
+        <div className="maker">
+          <span className="maker__name">JEONG MYEONG HAN</span>{' '}
+          <span className="maker__role">[BACKEND & REVIEWER / DATA & A.I]</span>
+        </div>
+        <h2 className="team__name">
+          {new Date().getFullYear()} GAEBL, All Rights Reserved.
+        </h2>
+      </InnerBox>
     </FooterWrapper>
   );
 }
